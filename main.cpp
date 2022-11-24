7d1266caa86ce4f389a2c93463c7795bd4c7d744
gitpher <jun_chois@naver.com>
2022-11-25 02:10:03 +0900

    first documentation on Qt

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <QCoreApplication>
 #include <QDebug>
 
+
+/*! Documentation for automated systems like Doxygen
+ * \brief main The starting point
+ * \param argc The argument count
+ * \param argv The argument
+ * \return int the exit value of the application
+ */
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
