a642a0045010b5a6654a72305b0b49de73e38762
gitpher <jun_chois@naver.com>
2022-11-25 02:19:43 +0900

    deleting documentation

--- main.cpp
+++ main.cpp
@@ -2,12 +2,6 @@
 #include <QDebug>
 
 
-/*! Documentation for automated systems like Doxygen
- * \brief main The starting point
- * \param argc The argument count
- * \param argv The argument
- * \return int the exit value of the application
- */
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
