44991b7b05aa1affe3c153643b6704b32ff80b08
gitpher <jun_chois@naver.com>
2022-11-25 12:00:20 +0900

    data type

--- main.cpp
+++ main.cpp
@@ -6,13 +6,30 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    bool lightSwitch = false;
+    int age = 24;
+    double height = 174.3;
 
-    qInfo() << "isOn: " << lightSwitch;
+    qInfo() << "Age: " << age;
+    qInfo() << "Height" << height;
 
-    lightSwitch = true;
+    age = (int) 24.8;
+    qInfo() << "New Age: " << age;
 
-    qInfo() << "isOn: " << lightSwitch;
+    // signed int
+    signed int dogs = -1;
+    qInfo() << "Dogs: " << dogs;
+
+    // unsigned int
+    unsigned int cats = -1;
+    qInfo() << "Cats: " << cats;
+
+    // sizeof()
+    qInfo() << "sizeof(dogs): " << sizeof(dogs) << " bytes";
+    qInfo() << "sizeof(cats): " << sizeof(cats) << " bytes";
+
+
+
+
 
     return a.exec();
 }
