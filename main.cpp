4388469e74e3d732b0855eb569edd97f7566dd6d
gitpher <jun_chois@naver.com>
2022-11-25 12:03:52 +0900

    constant

--- main.cpp
+++ main.cpp
@@ -6,28 +6,17 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+
+    /* practice */
+
     int age = 24;
-    double height = 174.3;
 
-    qInfo() << "Age: " << age;
-    qInfo() << "Height" << height;
+    const int id = 007;
 
-    age = (int) 24.8;
-    qInfo() << "New Age: " << age;
+    // id = 008;
 
-    // signed int
-    signed int dogs = -1;
-    qInfo() << "Dogs: " << dogs;
-
-    // unsigned int
-    unsigned int cats = -1;
-    qInfo() << "Cats: " << cats;
-
-    // sizeof()
-    qInfo() << "sizeof(dogs): " << sizeof(dogs) << " bytes";
-    qInfo() << "sizeof(cats): " << sizeof(cats) << " bytes";
-
-
+    qInfo() << "age: " << age;
+    qInfo() << "id: " << id;
 
 
 
