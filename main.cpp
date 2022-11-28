f0e4483194a5d6a3770330020842267fed5cc112
gitpher <jun_chois@naver.com>
2022-11-28 16:07:49 +0900

    function and flow control

--- main.cpp
+++ main.cpp
@@ -6,8 +6,19 @@
 #include <iostream>
 using namespace std;
 
-int multiply(int x, int y) {
-    return x * y;
+int calc(int offset, int age) {
+    // Validity Check
+    if (offset == 0) qFatal("Offset can not be zero");
+    if (age <= 0 || age > 120) qFatal("Invalid age");
+    return offset * age;
+}
+
+int catYears(int age) {
+    return calc(9, age);
+}
+
+int dogYears(int age) {
+    return calc(7, age);
 }
 
 
@@ -16,8 +27,13 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     /* playground */
+    int age(10);
+    qInfo() << "Enter your age:";
+    cin >> age;
 
-    qInfo() << multiply(2,44);
+    qInfo() << "dogYears(age)" << dogYears(age);
+    qInfo() << "catYears(age)" << catYears(age);
+
 
 
     return a.exec();
