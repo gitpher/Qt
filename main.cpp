477a824191b9cf50eae9dfc37e7e3470f9683c08
gitpher <jun_chois@naver.com>
2022-11-28 16:15:20 +0900

    overloading

--- main.cpp
+++ main.cpp
@@ -6,19 +6,12 @@
 #include <iostream>
 using namespace std;
 
-int calc(int offset, int age) {
-    // Validity Check
-    if (offset == 0) qFatal("Offset can not be zero");
-    if (age <= 0 || age > 120) qFatal("Invalid age");
-    return offset * age;
+void sayHello() {
+    qInfo("Hi");
 }
 
-int catYears(int age) {
-    return calc(9, age);
-}
-
-int dogYears(int age) {
-    return calc(7, age);
+void sayHello(QString name) {
+    qInfo() << "Hi" << name;
 }
 
 
@@ -27,13 +20,8 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     /* playground */
-    int age(10);
-    qInfo() << "Enter your age:";
-    cin >> age;
-
-    qInfo() << "dogYears(age)" << dogYears(age);
-    qInfo() << "catYears(age)" << catYears(age);
-
+    sayHello();
+    sayHello("Jinkee");
 
 
     return a.exec();
