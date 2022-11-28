f22c6d6f9c2d274d89c5d6885b95d541807dfc9e
gitpher <jun_chois@naver.com>
2022-11-28 15:39:02 +0900

    intro to functions

--- main.cpp
+++ main.cpp
@@ -1,32 +1,24 @@
 #include <QCoreApplication>
 #include <QDebug>
 
+/* for convenience */
 #include <array>
 #include <iostream>
 using namespace std;
+
+
+void test() {
+    qInfo() << "Hello from test()";
+}
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-
     /* playground */
 
-    int max(5);
-    QString task = "Plz take out the trash";
-    for (int i=1; i<50; i++) {
-        qInfo() << "Wife:" << task;
-        qWarning() << "Wife: I have asked" << i << "times!";
-
-        if (i>=max) {
-            qInfo() << "Husband: ok, ok, I'm going";
-            break;
-        }
-
-        qInfo() << "Husband: I will";
-    }
-
-    qInfo() << "Done";
+    test();
 
     return a.exec();
 }
