535b9aae77849fbfdf86b159fec0b9b5612ae5cd
gitpher <jun_chois@naver.com>
2022-11-29 22:12:35 +0900

    get object by reference & pointer

--- main.cpp
+++ main.cpp
@@ -6,26 +6,41 @@
 #include <iostream>
 using namespace std;
 
-struct Laptop {
-    int weight;
+/*
+ * This is not the way you do C++
+ *
+QObject get_val(QString name) {
+    QObject o;
+    o.setObjectName(name);
+    return o;
+}
 
-    double asKilograms() {
-        return weight * 0.453;
-    }
+QObject& get_ref(QString name) {
+    QObject o; // on the stack, c++ manages
+    o.setObjectName(name);
+    return o; // will be deleted
+}
+*/
 
-};
+QObject* get_ptr(QString name) {
+    QObject *o = new QObject(); // on the heap, I manages
+    o->setObjectName(name);
+    return o; // will not be deleted
+}
+
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
     /* playground */
+    QObject *o1 = get_ptr("Junwoo");
 
-    Laptop notebook;
-    notebook.weight = 5;
-    qInfo() << "pounds:" << notebook.weight;
-    qInfo() << "kilograms:" << notebook.asKilograms();
+    qInfo() << o1;
+    qInfo() << o1->objectName();
 
+    delete o1; // delete off o1 from the heap
 
 
     return a.exec();
